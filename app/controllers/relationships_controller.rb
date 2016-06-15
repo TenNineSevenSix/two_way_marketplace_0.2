c792ece26b29382da2578a73b324effb15660030
Alex Karolis <alexi2@protonmail.com>
2016-06-15 23:34:53 +1000

    wip

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -10,16 +10,32 @@ class RelationshipsController < ApplicationController
     redirect_to @requested_profile
   end
 
-  def relationship_request(requested_profile)
+  def index
     if current_user.has_role? :tutor
-      @profile = TutorProfile.find_by(user_id: current_user.id)
-      @relationship = @profile.relationships.create(student_profile: requested_profile)
-      @relationship.tutor_approved = true
-      @relationship.save
+      current_profile = TutorProfile.find(params[:id])
+      relationships = current_profile.relationships
+      student_profile_ids = []
+      relationships.each do |relationship|
+        student_profile_ids.push(relationship.student_profile_id)
+      end
+      @profiles = StudentProfile.where()
     else
-      @profile = StudentProfile.find_by(user_id: current_user.id)
-      @relationship = @profile.relationships.create(tutor_profile: requested_profile)
-      @relationship.save
+      current_profile = StudentProfile.find(params[:id])
     end
   end
+
+  private
+
+    def relationship_request(requested_profile)
+      if current_user.has_role? :tutor
+        @profile = TutorProfile.find_by(user_id: current_user.id)
+        @relationship = @profile.relationships.create(student_profile: requested_profile)
+        @relationship.tutor_approved = true
+        @relationship.save
+      else
+        @profile = StudentProfile.find_by(user_id: current_user.id)
+        @relationship = @profile.relationships.create(tutor_profile: requested_profile)
+        @relationship.save
+      end
+    end
 end
