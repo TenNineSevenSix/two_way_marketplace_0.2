285850c00375ea7c51b0f1dd9940c3c7f603888a
Alex Karolis <alexi2@protonmail.com>
2016-06-15 12:12:34 +1000

    remove last two migrations

--- db/migrate/20160614232550_remove_profile_images_from_student_profiles.rb
+++
@@ -1,5 +0,0 @@
-class RemoveProfileImagesFromStudentProfiles < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :student_profiles, :profile_images, :string
-  end
-end
--- db/migrate/20160614232638_remove_profile_images_from_tutor_profiles.rb
+++
@@ -1,5 +0,0 @@
-class RemoveProfileImagesFromTutorProfiles < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :tutor_profiles, :profile_images, :string
-  end
-end
